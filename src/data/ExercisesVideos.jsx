e5d4c90c328ac38d2f93bf94ae3376e0da6d0534
Carolina Lunas <carolinalunasfarah@gmail.com>
2024-07-23 14:31:38 -0400

    update page title and videos from mindfulness and exercise data

--- src/data/ExercisesVideos.jsx
+++ src/data/ExercisesVideos.jsx
@@ -1,15 +1,15 @@
 export const ExercisesVideos = [
     {
         name: "Sesión 1",
-        video_src: "https://www.youtube.com/embed/BePLp7lqnxA",
+        video_src: "https://www.youtube.com/embed/SibjPw9Khz4",
     },
     {
         name: "Sesión 2",
-        video_src: "https://www.youtube.com/embed/BePLp7lqnxA",
+        video_src: "https://www.youtube.com/embed/sBXkZlQPC2U",
     },
     {
         name: "Sesión 3",
-        video_src: "https://www.youtube.com/embed/BePLp7lqnxA",
+        video_src: "https://www.youtube.com/embed/FFHwoIQmFEA",
     },
 ];
 
--- src/data/MindfulnessVideos.jsx
+++ src/data/MindfulnessVideos.jsx
@@ -1,15 +1,15 @@
 export const MindfulnessVideos = [
     {
         name: "Sesión 1",
-        video_src: "https://www.youtube.com/embed/dQw4w9WgXcQ",
+        video_src: "https://www.youtube.com/embed/YEufx0cqjmo",
     },
     {
         name: "Sesión 2",
-        video_src: "https://www.youtube.com/embed/dQw4w9WgXcQ",
+        video_src: "https://www.youtube.com/embed/DD9K9i9e3AM",
     },
     {
         name: "Sesión 3",
-        video_src: "https://www.youtube.com/embed/dQw4w9WgXcQ",
+        video_src: "https://www.youtube.com/embed/LAoWA1Z8MZE",
     },
 ];
 
